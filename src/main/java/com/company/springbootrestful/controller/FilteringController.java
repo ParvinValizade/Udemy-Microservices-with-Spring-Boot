fe616932a19d4eee857d1648b5a12057f00a2c05
ParvinValizade <parvin.valizade@mail.ru>
2023-01-10 10:29:16 +0400

    Static filtering for list

--- src/main/java/com/company/springbootrestful/controller/FilteringController.java
+++ src/main/java/com/company/springbootrestful/controller/FilteringController.java
@@ -5,12 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/filtering")
 public class FilteringController {
 
    @GetMapping
-    private SomeBean filtering(){
+    public SomeBean filtering(){
         return new SomeBean("value1","value2","value3","value4");
     }
+
+    @GetMapping("/list")
+    public List<SomeBean> filteringList(){
+       return Arrays.asList(new SomeBean("value1","value2","value3","value4"),new SomeBean("value1","value2","value3","value4"));
+    }
+
+
 }
