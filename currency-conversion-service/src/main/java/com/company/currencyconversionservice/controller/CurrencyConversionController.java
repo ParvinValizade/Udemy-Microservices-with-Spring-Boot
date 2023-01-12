274f6ef02cf571eab89d9b556ac782b8287f1b20
ParvinValizade <parvin.valizade@mail.ru>
2023-01-12 17:50:42 +0400

    currency-conversion-ms

--- currency-conversion-service/src/main/java/com/company/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/company/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,9 +1,21 @@
 package com.company.currencyconversionservice.controller;
 
+import com.company.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
+
+    @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
+                                                          @PathVariable String to,
+                                                          @PathVariable BigDecimal quantity) {
+        return new CurrencyConversion(1L, from,to,quantity,BigDecimal.ONE,BigDecimal.ONE,"");
+    }
 }
